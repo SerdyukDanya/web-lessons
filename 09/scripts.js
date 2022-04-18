707f9a1e863bf7fbd2b8217db4cdf2e86d89f3e6
Danya <serdyuk.danyil@kmrf.kiev.ua>
2022-04-18 16:31:13 +0300

    add 09

---
+++ 09/scripts.js
@@ -0,0 +1,5 @@
+document.write('text <br>');
+document.write('<i>text</i> <br>');
+let str = 'text';
+document.write('<i>' + str + '</i>' + '<br>');
+document.write(1 +'<br>' + 2 + '<br>'+ 3 + '<br>'+ 4 + '<br>'+ 5);
